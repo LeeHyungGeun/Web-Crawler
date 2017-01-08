9c3b9147db50bc64f71cbf5f8babb372fd67b0bc
LeeHyungGeun <lhg4031@gmail.com>
2017-01-08 21:11:40 +0800

    Server Host and Port Config

---
+++ configs/server.config.js
@@ -0,0 +1,15 @@
+var host = 'localhost';
+module.exports = {
+    webpack: {
+        host: host,
+        port: 8080
+    },
+    bs: {
+        host: host,
+        port: 8000
+    },
+    server: {
+        host: host,
+        port: 9000
+    }
+};
