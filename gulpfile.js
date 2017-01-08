6c971caffbe15115e93fef2d0080f1cefc0591ac
LeeHyungGeun <lhg4031@gmail.com>
2017-01-08 21:10:41 +0800

    add BrowserSync and nodemon

--- gulpfile.js
+++ gulpfile.js
@@ -2,13 +2,19 @@ var gulp = require('gulp');
 var webpack = require('webpack');
 var WebpackDevServer = require('webpack-dev-server');
 
-var $ = require('gulp-load-plugins');
+var browserSync = require('browser-sync');
+var nodemon = require('gulp-nodemon');
+
+var gutil = require('gulp-util');
 
 var configs = {
     webpack: {
         dev: require('./configs/webpack.config.dev.js'),
         prod: require('./configs/webpack.config.prod.js')
-    }
+    },
+    server: require('./configs/server.config.js'),
+    nodemon: require('./configs/nodemon.config.js'),
+    bs: require('./configs/bs.config.js')
 };
 
 gulp.task('prod', prod);
@@ -16,16 +22,17 @@ gulp.task('dev', dev);
 gulp.task('default', dev);
 
 function dev() {
-    var host = 'localhost';
-    var port = 8000;
     new WebpackDevServer(webpack(configs.webpack.dev), configs.webpack.dev.devServer)
-    .listen(port, host, function(err) {
+    .listen(configs.server.webpack.port, configs.server.webpack.host, function(err) {
         if (err) {
             throw new $.util.PluginError('webpack-dev-server', err);
-            $.util.log('[webpack-dev-server]', 'http://' + host + ':' + port + '/webpack-dev-server');
         }
         else {
+            gutil.log('[webpack-dev-server]', 'http://' + configs.server.webpack.host + ':' + configs.server.webpack.port + '/webpack-dev-server');
             // TODO
+            bsServer();
+            nodemonServer();
+            
         }
     });
 }
@@ -33,10 +40,22 @@ function prod(callback) {
     webpack(configs.webpack.prod, function(err, stats) {
         if (err) {
             throw new $.util.PluginError('webpack', err);
-            $.util.log('[webpack]', stats.toString({
+            gutil.log('[webpack]', stats.toString({
                 // output options
             }));
             callback();
         }
     })
+}
+
+function bsServer() {
+    browserSync(configs.bs, function(err) {
+        if (err) {
+            gutil.log(gutil.colors.red('[BS]: ' + err));
+        }
+    });
+}
+
+function nodemonServer() {
+    nodemon(configs.nodemon);
 }
